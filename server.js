58940fda2fe7061a0147108f187b707cd36e0509
Kory Nelson <kory.nelson99@gmail.com>
2020-09-28 21:38:41 -0700

    udpated server.js

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
+const path = require("path");
+const app = express();
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
