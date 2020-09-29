db762bb558110cab7193d28d6edd93983d2c4531
Kory Nelson <kory.nelson99@gmail.com>
2020-09-28 21:59:28 -0700

    update file path for index.html

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require("./routes/api-routes.js")(app);
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
   
 
