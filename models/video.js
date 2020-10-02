0c062aee3ee448c9b10e1e2758c54821ea78c601
birol ezgin <60679626+birolezgin@users.noreply.github.com>
2020-10-01 20:33:40 -0700

    Update video.js

--- models/video.js
+++ models/video.js
@@ -1,13 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
-    var Video = sequelize.define("Video", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1,140]
-        }
-      },
-    });
-    return Video;
-  };
+  var Video = sequelize.define("Video", {
+    text: DataTypes.STRING,
+    complete: DataTypes.BOOLEAN
+  });
+  return Video;
+};
+
   
