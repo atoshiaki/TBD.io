f80961eeed07c6bdcd45effebe6eef4def9a1753
birol ezgin <60679626+birolezgin@users.noreply.github.com>
2020-10-01 20:44:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-// Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
-const path = require("path");
-const app = express();
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
 
-// Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Requiring our models for syncing
-const db = require("./models");
 
 // Static directory
 app.use(express.static("public"));
@@ -21,16 +25,7 @@ app.use(express.static("public"));
 // =============================================================
 require("./routes/api-routes.js")(app);
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-  
-
-// Starts the server to begin listening
-// =============================================================
-
-  // Syncing our sequelize models and then starting our Express app
+// Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
