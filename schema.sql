488d8b493880ed07249f6a05b45f0a913f48902d
birol ezgin <60679626+birolezgin@users.noreply.github.com>
2020-10-01 20:41:25 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,13 +1,4 @@
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS streamer;
--- Creates the "blogger" database --
-CREATE DATABASE streamer;
-
-USE streamer;
-
-CREATE TABLE videoId
-(
-	name VARCHAR(30)
-    );
-    
-INSERT INTO videoId (name) VALUES ("hLMjnm1Bm_Y");
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS videolist;
+-- Creates the "todolist" database --
+CREATE DATABASE videolist;
