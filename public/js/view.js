e75a08766282cda5e01a5e4e879e3f40e4af9049
Kory Nelson <kory.nelson99@gmail.com>
2020-10-01 21:17:41 -0700

    delete static youtube videos and add heroku api

--- public/js/view.js
+++ public/js/view.js
@@ -147,69 +147,22 @@ $(document).ready(function(){
     
 })
 
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-console.log(tag)
-tag.src = "https://www.youtube.com/iframe_api";
-console.log(tag.src)
-//Pull videos from youtube api
-var firstScriptTag = document.getElementsByTagName('script')[0];
-console.log(firstScriptTag)
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+////////////////////////////////////////////////////////////////////////////////
+//////////////////add twitch////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-onYouTubeIframeAPIReady();
-
-function onYouTubeIframeAPIReady() {
-player = new YT.Player('player', {
-    height: '290',
-    width: '440',
-    videoId: '4q3mF9ZLGNM',
-    events: {
-    'onReady': onPlayerReady,
-    'onStateChange': onPlayerStateChange
-    }
+var embed = new Twitch.Embed("twitch-embed", {
+  width: 854,
+  height: 480,
+  channel: "monstercat",
+  layout: "video",
+  autoplay: false,
+  // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+  parent: ["https://oku-no-stream.herokuapp.com/"]
 });
-player = new YT.Player('player2', {
-    height: '290',
-    width: '440',
-    videoId: '9r-pPADtXuc',
-    events: {
-    'onReady': onPlayerReady,
-    'onStateChange': onPlayerStateChange
-    }
+
+embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+  var player = embed.getPlayer();
+  player.play();
 });
-player = new YT.Player('player3', {
-    height: '290',
-    width: '440',
-    videoId: 'hLMjnm1Bm_Y',
-    events: {
-    'onReady': onPlayerReady,
-    'onStateChange': onPlayerStateChange
-    }
-});
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-
-function onPlayerStateChange(event) {
-if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-}
-}
-
-function stopVideo() {
-player.stopVideo();
-}
 
