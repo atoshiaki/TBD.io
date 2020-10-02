b6b1b028b99202ba133e8b1dcc6cb30785ea6525
birol ezgin <60679626+birolezgin@users.noreply.github.com>
2020-10-01 20:40:30 -0700

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,41 +5,74 @@
 // Dependencies
 // =============================================================
 
-// Requiring our Todo model
+// Requiring our Video model
 var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({})
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+ // GET route for getting all of the videos
+ app.get("/api/videos", function(req, res) {
+  // findAll returns all entries for a table when used with no options
+  db.Video.findAll({}).then(function(dbVideo) {
+    // We have access to the videos as an argument inside of the callback function
+    res.json(dbVideo);
   });
+});
 
- // POST route for saving a new post
- app.post("/api/posts", function(req, res) {
-  console.log(req.body);
-  db.Post.create({
-    name: req.params.name
+// POST route for saving a new video
+app.post("/api/videos", function(req, res) {
+  // create takes an argument of an object describing the item we want to
+  // insert into our table. In this case we just we pass in an object with a text
+  // and complete property (req.body)
+  db.Video.create({
+    text: req.body.text,
+    complete: req.body.complete
+  }).then(function(dbVideo) {
+    // We have access to the new video as an argument inside of the callback function
+    res.json(dbVideo);
   })
-    .then(function(dbPost) {
-      res.json(dbPost);
+    .catch(function(err) {
+    // Whenever a validation or flag fails, an error is thrown
+    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+      res.json(err);
     });
 });
 
-// DELETE route for deleting posts
-app.delete("/api/posts/:name", function(req, res) {
-  db.Post.destroy({
+// DELETE route for deleting videos. We can get the id of the video to be deleted from
+// req.params.id
+app.delete("/api/videos/:id", function(req, res) {
+  // We just have to specify which video we want to destroy with "where"
+  db.Video.destroy({
     where: {
-      name: req.params.name
+      id: req.params.id
     }
+  }).then(function(dbVideo) {
+    res.json(dbVideo);
+  });
+
+});
+
+// PUT route for updating videos. We can get the updated video data from req.body
+app.put("/api/videos", function(req, res) {
+
+  // Update takes in an object describing the properties we want to update, and
+  // we use where to describe which objects we want to update
+  db.Video.update({
+    text: req.body.text,
+    complete: req.body.complete
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then(function(dbVideo) {
+    res.json(dbVideo);
   })
-    .then(function(dbPost) {
-      res.json(dbPost);
+    .catch(function(err) {
+    // Whenever a validation or flag fails, an error is thrown
+    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+      res.json(err);
     });
   });
 };
