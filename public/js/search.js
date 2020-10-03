94fd0f8c21610805535f692c962fd13e28c98c33
Kory Nelson <kory.nelson99@gmail.com>
2020-10-02 20:24:19 -0700

    linked yt search to search box and console log data

--- public/js/search.js
+++ public/js/search.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   var API_KEY = "AIzaSyAQmv6tfX2O--XdCmHzgIP-RJ_KcoMUjxA";
 
-runSearch(API_KEY)
+  $("#addVideo").on("click", function(){
+    runSearch(API_KEY)
+  });
 
   function runSearch(key){
+    var searchInput = $("#srcInput").val();
+    console.log(searchInput);
     $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxresults=10&q=halo&eventType=live",
     function(data){
-      console.log(data);
+      console.log(data.items[0]);
     }
     )
   }
