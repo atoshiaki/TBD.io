eed261cd336852aa436b03f98398df669672eceb
Kory Nelson <kory.nelson99@gmail.com>
2020-10-02 21:31:00 -0700

    added jpeg for each search result to the bottom of the page

--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,54 @@
 $(document).ready(function(){
+
+  //we want to show results in the results container so we will make a variable to work with the results container
+  var $resultsContainer = $(".results-container");
+  //also make a variable to pull search input
+  var $searchInput = $("input.new-item").val();
+  //API key for YouTube
   var API_KEY = "AIzaSyAQmv6tfX2O--XdCmHzgIP-RJ_KcoMUjxA";
 
   $("#addVideo").on("click", function(){
     runSearch(API_KEY)
   });
 
+
+
+  //define funtions below here
+
+  //Thhis function runs the YouTube search
   function runSearch(key){
-    var searchInput = $("#srcInput").val();
-    console.log(searchInput);
+    console.log($searchInput);
     $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxresults=10&q=halo&eventType=live",
-    function(data){
-      console.log(data.items[0]);
-    }
-    )
+      function(data){
+        console.log(data.items);
+        showResults(data.items);
+      });
   }
+
+  //This function just console logs the results from the YouTube search
+  function showResults(data){
+    console.log(data)
+    data.forEach(element => {
+      console.log(element.snippet.thumbnails.default.url)
+      
+      $resultsContainer.prepend(createResult(element.snippet.thumbnails.default.url))
+    });
+    };
+
+  // This function constructs a results-item row
+  function createResult(video) {
+    var $newInputRow = $(
+      [
+        "<img src='"+ video +"'id='currentIcon'>",
+      ].join("")
+    );
+    console.log($newInputRow)
+    return $newInputRow;
+  }
+ 
   
 
+//end of document.ready()
 })
 
 
