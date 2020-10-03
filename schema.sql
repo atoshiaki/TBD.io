346144dc7891da5736c083b4d4101dcd752e311f
birol ezgin <60679626+birolezgin@users.noreply.github.com>
2020-10-02 20:02:24 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Drops the todolist if it exists currently --
+-- Drops the videolist if it exists currently --
 DROP DATABASE IF EXISTS videolist;
--- Creates the "todolist" database --
+-- Creates the "videolist" database --
 CREATE DATABASE videolist;
